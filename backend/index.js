9995d0b794c5da14f3d72872803219f3c8aba53e
Vidhi Singh <vidhip.ug22.ec@nitp.ac.in>
2025-11-09 19:18:32 +0530

    Specified routes in index.js file

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
 const express=require("express");
 const cors=require("cors");
 const { connectdb } = require("./config/connectmongodb");
+const signin=require('./routes/authroutes/signin');
+const signup=require('./routes/authroutes/signup');
+const bookslot=require('./routes/slotroutes/bookslot');
+const currentslot=require('./routes/slotroutes/currentslot');
+const bookinghistory=require('./routes/slotroutes/bookinghistory');
+
 
 dotenv.config();
 
@@ -12,8 +18,11 @@ app.use(express.json());
 
 connectdb();
 
-app.use("/api/authroutes",authroutes);
-app.use("/api/slotroutes",slotroutes);
+app.use("/",signin);
+app.use("/",signup);
+app.use("/",bookslot);
+app.use("/",currentslot);
+app.use("/",bookinghistory);
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server listening on port ${process.env.PORT}`);
--- backend/service/hashpassword.js
+++ backend/service/hashpassword.js
@@ -1,6 +1,6 @@
 const bcrypt=require('bcrypt');
 async function hashpassword(password)
 {
-    return await bcrypt.hash(10,password);
+    return await bcrypt.hash(12,password);
 }
 module.exports=hashpassword;
